466b312ef220d1b4ec67712894e248f41ef14cc1
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-11 20:58:40 +0000

    added package details

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openmc_data_downloader",
+    version="0.0.1",
+    summary="Download cross section h5 files for use in OpenMC",
+    author="Jonathan Shimwell",
+    author_email="mail@jshimwell.com",
+    description="A tool for selectively downloading h5 files for specified isotopes / elements from your libraries of choice",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/openmc-data-storage/openmc_data_downloader",
+    packages=setuptools.find_packages(),
+    zip_safe=True,
+    package_dir={"openmc_data_downloader": "openmc_data_downloader"},
+    package_data={
+        "openmc_data_downloader": [
+            "requirements.txt",
+            "README.md",
+            "LICENSE",
+        ]
+    },
+    tests_require=["pytest-cov", "pytest-runner"],
+    install_requires=[
+        "pandas",
+        # 'openmc' when pip install is available
+    ],
+)
