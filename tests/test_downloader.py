eed24fde66c4c2a6504fdb64017a19efde3775ae
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-12 21:01:50 +0000

    element expansion with inbuild dict

---
+++ tests/test_downloader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+__author__ = "Jonathan Shimwell"
+
+
+import unittest
+
+
+
+class test_command_line_usage(unittest.TestCase):
+    def test_library_preference(self):
+        pass
+       
