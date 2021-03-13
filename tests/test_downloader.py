3453f84355078e52221ab7e1ed80b7c2bc38e178
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-13 15:40:06 +0000

    added unit tests for examping materials xml and mat objects

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -3,11 +3,51 @@
 __author__ = "Jonathan Shimwell"
 
 
+import os
 import unittest
 
+from openmc import Material, Materials
+from openmc_data_downloader import (expand_materials_to_isotopes,
+                                    expand_materials_xml_to_isotopes)
 
 
 class test_command_line_usage(unittest.TestCase):
-    def test_library_preference(self):
-        pass
-       
+
+    def expand_materials_from_object_list_with_single_mat(self):
+
+        my_mat = Material()
+        my_mat.add_nuclide('Pu239', 3.7047e-2)
+        my_mat.add_nuclide('Pu240', 1.7512e-3)
+        my_mat.add_nuclide('Pu241', 1.1674e-4)
+
+        assert expand_materials_to_isotopes([my_mat]) == ['Pu239', 'Pu240', 'Pu241']
+
+    def expand_materials_from_object_list_with_multiple_mat(self):
+
+        my_mat1 = Material()
+        my_mat1.add_nuclide('Li6', 0.5)
+        my_mat1.add_nuclide('Li7', 0.25)
+
+        my_mat2 = Material()
+        my_mat2.add_nuclide('Al27', 0.25)
+
+        assert expand_materials_to_isotopes([my_mat1, my_mat2]) == ['Li6', 'Li7', 'Al27']
+
+    def test_expand_material_xmls_with_list_input(self):
+
+        my_mat = Material()
+        my_mat.add_nuclide('Be9', 0.5)
+        os.system('rm materials.xml')
+        Materials([my_mat]).export_to_xml()
+
+        assert expand_materials_xml_to_isotopes(['materials.xml']) == ['Be9']
+
+    def test_expand_material_xmls_with_str_input(self):
+
+        my_mat = Material()
+        my_mat.add_element('Al', 0.5)
+        os.system('rm materials.xml')
+        Materials([my_mat]).export_to_xml()
+
+        assert expand_materials_xml_to_isotopes('materials.xml') == ['Al27']
+
