cd2942bf734838ea6a1a58d47b2e8d35e8cee948
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-13 16:29:22 +0000

    added tests for command line usage

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -5,6 +5,7 @@ __author__ = "Jonathan Shimwell"
 
 import os
 import unittest
+from pathlib import Path
 
 from openmc import Material, Materials
 from openmc_data_downloader import (expand_materials_to_isotopes,
@@ -71,3 +72,45 @@ class test_command_line_usage(unittest.TestCase):
             ValueError,
             incorrect_libraries_string
         )
+
+    def test_correct_files_from_command_line_usage_1(self):
+        os.system('rm *.h5')
+        os.system('openmc_data_downloader -l TENDL_2019 -i H1')
+
+        assert Path('TENDL_2019_H1.h5').is_file()
+        assert len(list(Path('.').glob('*.h5'))) == 1
+
+    def test_correct_files_from_command_line_usage_2(self):
+        os.system('rm *.h5')
+        os.system('openmc_data_downloader -l TENDL_2019 -e F')
+
+        assert Path('TENDL_2019_F19.h5').is_file()
+        assert len(list(Path('.').glob('*.h5'))) == 1
+
+    def test_correct_files_from_command_line_usage_3(self):
+
+        os.system('rm *.h5')
+
+        os.system('openmc_data_downloader -l ENDFB_71_NNDC -e Co Y')
+
+        assert Path('ENDFB_71_NNDC_Co59.h5').is_file()
+        assert Path('ENDFB_71_NNDC_Y89.h5').is_file()
+        assert len(list(Path('.').glob('*.h5'))) == 2
+
+    def test_correct_files_from_command_line_usage_4(self):
+
+        os.system('rm *.h5 materials.xml')
+
+        my_mat = Material()
+        my_mat.add_element('Nb', 0.5)
+        my_mat.add_nuclide('Cs133', 0.5)
+        os.system('rm materials.xml')
+        Materials([my_mat]).export_to_xml()
+
+        os.system('openmc_data_downloader -l TENDL_2019 -m materials.xml')
+
+        assert expand_materials_xml_to_isotopes(['materials.xml']) == ['Nb93', 'Cs133']
+
+        assert Path('TENDL_2019_Nb93.h5').is_file()
+        assert Path('TENDL_2019_Cs133.h5').is_file()
+        assert len(list(Path('.').glob('*.h5'))) == 2
