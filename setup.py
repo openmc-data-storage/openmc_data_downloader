b82f5da1f2d20f0828a26fccc5e83c40b4ae20f4
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-15 08:39:33 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openmc_data_downloader",
-    version="0.0.9",
+    version="0.0.10",
     summary="Download cross section h5 files for use in OpenMC",
     author="Jonathan Shimwell",
     author_email="mail@jshimwell.com",
