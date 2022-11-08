8454036b5b24f13e16c0c646f0b4ff579b1e1754
shimwell <shimwell@users.noreply.github.com>
2022-11-08 08:22:02 +0000

    [skip ci] Apply formatting changes

--- src/openmc_data_downloader/terminal_cmd.py
+++ src/openmc_data_downloader/terminal_cmd.py
@@ -11,7 +11,11 @@ generation of custom cross_section.xml files
 import argparse
 from pathlib import Path
 
-from openmc_data_downloader import just_in_time_library_generator, LIB_OPTIONS, SAB_OPTIONS
+from openmc_data_downloader import (
+    just_in_time_library_generator,
+    LIB_OPTIONS,
+    SAB_OPTIONS,
+)
 
 
 def main():
@@ -19,62 +23,71 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '-l', '--libraries',
-        choices=LIB_OPTIONS, nargs='*',
+        "-l",
+        "--libraries",
+        choices=LIB_OPTIONS,
+        nargs="*",
         help="The nuclear data libraries to search through when searching for \
         cross sections. Multiple libaries are acceptable and will be \
         preferentially utilized in the order provided",
         default=[],
-        required=True
+        required=True,
     )
     parser.add_argument(
-        '-i', '--isotopes',
-        nargs='*',
+        "-i",
+        "--isotopes",
+        nargs="*",
         default=[],
-        help="The isotope or isotopes to download, name of isotope e.g. 'Al27' or keyword 'all' or 'stable'"
+        help="The isotope or isotopes to download, name of isotope e.g. 'Al27' or keyword 'all' or 'stable'",
     )
     parser.add_argument(
-        '-s',
-        '--sab',
-        nargs='*',
+        "-s",
+        "--sab",
+        nargs="*",
         default=[],
-        help="The SaB cross sections to download. Options include " +
-        " ".join(SAB_OPTIONS))
+        help="The SaB cross sections to download. Options include "
+        + " ".join(SAB_OPTIONS),
+    )
 
     parser.add_argument(
-        '-e', '--elements', nargs='*',
+        "-e",
+        "--elements",
+        nargs="*",
         default=[],
-        help="The element or elements to download, name of element e.g. 'Al' or keyword 'all' or 'stable'"
+        help="The element or elements to download, name of element e.g. 'Al' or keyword 'all' or 'stable'",
     )
     parser.add_argument(
-        '-p', '--particles', nargs='*',
-        default=['neutron'],
-        choices=['neutron', 'photon'],
+        "-p",
+        "--particles",
+        nargs="*",
+        default=["neutron"],
+        choices=["neutron", "photon"],
         help="The particle to download",
     )
     parser.add_argument(
-        '-m', '--materials_xml',
-        nargs='*',
+        "-m",
+        "--materials_xml",
+        nargs="*",
         default=[],
         help="The filename of the materials.xml file to \
-            provide cross sections for"
+            provide cross sections for",
     )
     parser.add_argument(
-        '-d', '--destination',
-        type=Path, default=None,
-        help='Directory to create new library in'
-    )
-
-    parser.add_argument(
-        '--overwrite',
-        action='store_true',
-        help='Exiting files will be overwritten'
+        "-d",
+        "--destination",
+        type=Path,
+        default=None,
+        help="Directory to create new library in",
     )
 
     parser.add_argument(
-        '--no-overwrite',
-        action='store_false',
-        help='Exiting files will not be overwritten'
+        "--overwrite", action="store_true", help="Exiting files will be overwritten"
+    )
+
+    parser.add_argument(
+        "--no-overwrite",
+        action="store_false",
+        help="Exiting files will not be overwritten",
     )
 
     parser.set_defaults(overwrite=False)
@@ -89,8 +102,9 @@ def main():
         materials_xml=args.materials_xml,
         particles=args.particles,
         set_OPENMC_CROSS_SECTIONS=False,
-        overwrite=args.overwrite
+        overwrite=args.overwrite,
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
